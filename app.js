3e2dd598268de637316831e59b681dded6dfd869
Geoffrey Laville <laville.geoffrey@gmail.com>
2021-10-16 00:10:33 +0200

    comments & corrections

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
+// require express
 const express = require("express");
-
+// create server
 const app = express();
 
+
+// Identify public folder for static files
 app.use(express.static("public"));
 
-app.get("/home", (request, response, next) => {
+
+// first home route
+app.get("/", (request, response, next) => {
   response.sendFile(__dirname + "/views/home.html");
 });
 
+// second route to /about
 app.get("/about", (request, response, next) => {
   response.sendFile(__dirname + "/views/about.html");
 });
 
+// third route to /wirks
 app.get("/works", (request, response, next) => {
   response.sendFile(__dirname + "/views/works.html");
 });
 
+
+// start server on port 3000
 app.listen(3000);
